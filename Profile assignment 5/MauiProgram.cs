ce1a302e030784d11516fec1f28f648a5a5ddddc
psudobeast <maropengsmart@gmail.com>
2026-04-15 08:43:04 +0200

    Add project files.

---
+++ Profile assignment 5/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Profile_assignment_5
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+            {
+#if ANDROID
+        handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#elif IOS || MACCATALYST
+        handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+            });
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+            return builder.Build();
+         }
+    }
+}
---
+++ Profile assignment 5/Model/Profile.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+public class Profile
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string EmailAddress { get; set; }
+    public string Bio { get; set; }
+
+    // Optional: Store Profile Picture as Base64 string
+    public string ProfilePictureBase64 { get; set; }
+
+    // Method to serialize Profile object to JSON string
+    public string ToJson()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+
+    // Static method to deserialize JSON string to Profile object
+    public static Profile FromJson(string json)
+    {
+        return JsonSerializer.Deserialize<Profile>(json);
+    }
+
+}
